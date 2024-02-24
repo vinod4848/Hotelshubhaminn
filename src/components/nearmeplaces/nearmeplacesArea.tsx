9b373300ae7842b7633cca6f977b8a77dba3d29b
vinod4848 <vinodku4848@gmail.com>
2024-02-24 16:16:14 +0530

    add card

--- src/components/nearmeplaces/nearmeplacesArea.tsx
+++ src/components/nearmeplaces/nearmeplacesArea.tsx
@@ -7,14 +7,18 @@ const NeaarmeArea = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-3 mb-4">
-              <div className="card">
+              <div className="card position-relative">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://blog-content.ixigo.com/wp-content/uploads/2022/09/23Sept-Feat-Final.jpeg"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 1</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Mumbai Airport</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    11.7 km
+                  </button>
                 </div>
               </div>
             </div>
@@ -22,12 +26,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipNujntUuuCJmWUw8dR4MqN8LaScLfd7RVa4rYPl=w408-h306-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 2</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Nahur station</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    1.4Km
+                  </button>
                 </div>
               </div>
             </div>
@@ -35,12 +43,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipP9uofGcEA0WyA-WzLKBx6Go1xSwGMVWa-nVPzo=w426-h240-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 3</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Dominos Pizza</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    1.1Km
+                  </button>
                 </div>
               </div>
             </div>
@@ -48,12 +60,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipMr2rV5QmjlEfCUhd4VF5OItTyxhBQI7pjEqpol=w408-h306-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 4</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Shivdhara Tours & Trekkers</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    1.5Km
+                  </button>
                 </div>
               </div>
             </div>
@@ -61,12 +77,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipPORNd1Js1YDHBDEPOGz90A5ElnwZ3PwarkqogN=w408-h272-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 5</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Sanjay Gandhi National Park</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    2.2Km
+                  </button>
                 </div>
               </div>
             </div>
@@ -74,12 +94,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipPboRxuwJiHCAqX4Ag81FG3tMtvAsnYIZfjW4Sd=w427-h240-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 6</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Nine Cuisine Family Resto Bar</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    97.M
+                  </button>
                 </div>
               </div>
             </div>
@@ -87,12 +111,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipOiPhB3UDBueWiIqgRBO5vGRak6ysHA2G_wKChr=w408-h297-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 7</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Mandarin Oak Bhandup</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    900.M
+                  </button>
                 </div>
               </div>
             </div>
@@ -100,12 +128,16 @@ const NeaarmeArea = () => {
             <div className="col-md-3 mb-4">
               <div className="card">
                 <img
-                  src="https://table.skift.com/wp-content/uploads/2019/01/2_Dominos-Store_Eugene-OR-e1546984571327.jpg"
+                  src="https://lh5.googleusercontent.com/p/AF1QipOw95mXjYNe9ZMaa0-0iWK-xOX84QakNGT9drfF=w350-h139-p-k-no"
                   className="card-img-top"
                   alt=""
+                  style={{ width: "328px", height: "220px" }}
                 />
-                <div className="card-body">
-                  <h5 className="card-title">Dominos 8</h5>
+                <div className="card-body text-center">
+                  <h5 className="card-title">Sameer Hill</h5>
+                  <button className="btn btn-danger position-absolute top-0 end-0 m-2">
+                    6.5Km
+                  </button>
                 </div>
               </div>
             </div>
