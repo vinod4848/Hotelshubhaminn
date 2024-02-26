01c5b75caf48fa9fd21c7828d7edc635ee7b644a
vinod4848 <vinodku4848@gmail.com>
2024-02-26 12:13:57 +0530

    chenge price page of abaut us

--- src/components/offers/OfferMain.tsx
+++ src/components/offers/OfferMain.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import Breadcrumb from '../common/breadcrumb/Breadcrumb';
-import BookingRoomSec from '../room-2/BookingRoomSec';
-import FacilitysOneSec from '../Home-3/FacilitysOneSec';
-import PricingPlanSec from '../Home-4/PricingPlanSec';
-import LatestNewsSec from '../Home-4/LatestNewsSec';
-import OfferSec from './OfferSec';
+import React from "react";
+import Breadcrumb from "../common/breadcrumb/Breadcrumb";
+import FacilitysOneSec from "../Home-3/FacilitysOneSec";
+import PricingPlanSec from "../Home-4/PricingPlanSec";
+import OfferSec from "./OfferSec";
 
 const OfferMain = () => {
-    return (
-        <>
-            <Breadcrumb title ="Promotions & Offers"/>
-            <OfferSec />
-            <BookingRoomSec/>
-            <FacilitysOneSec/>
-            <PricingPlanSec classbg="pricing-plan-area pricing-spacing theme-bg-2" classbgDiv="bd-price-item white-bg"/>
-            <LatestNewsSec/>
-        </>
-    );
+  return (
+    <>
+      <Breadcrumb title="Promotions & Offers" />
+      <OfferSec />
+      <FacilitysOneSec />
+      <PricingPlanSec
+        classbg="pricing-plan-area pricing-spacing theme-bg-2"
+        classbgDiv="bd-price-item white-bg"
+      />
+    </>
+  );
 };
 
 export default OfferMain;
