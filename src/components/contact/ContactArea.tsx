ee098eb3821e0c1c35fa8dcd72de62e3ba273a36
vinod4848 <vinodku4848@gmail.com>
2024-02-27 13:28:16 +0530

    chenge text color

--- src/components/contact/ContactArea.tsx
+++ src/components/contact/ContactArea.tsx
@@ -95,7 +95,7 @@ const ContactArea = () => {
             <div className="col-xl-6 mb-60">
               <div className="bd-contact-map">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14596.899807208923!2d90.3654215!3d23.8461445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1673765392297!5m2!1sen!2sbd"
+                  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3768.9582305776707!2d72.93456397440688!3d19.153305582067315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDA5JzExLjkiTiA3MsKwNTYnMTMuNyJF!5e0!3m2!1sen!2sin!4v1709020347509!5m2!1sen!2sin"
                   style={{ border: 0 }}
                   allowFullScreen={true}
                   loading="lazy"
