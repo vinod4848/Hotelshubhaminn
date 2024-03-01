049b593502075f31e2b03c48d8e6f0ee23fd23ca
vinod4848 <vinodku4848@gmail.com>
2024-03-01 18:29:09 +0530

    Email send function add

--- src/form/BookingForm.tsx
+++ src/form/BookingForm.tsx
@@ -2,30 +2,96 @@ import Preloader from "@/components/common/Preloader";
 import { AppContext } from "@/contextApi/AppProvider";
 import roomAndSuites from "@/data/roomAndSuitesData";
 import { AppContextType } from "@/interFace/interFace";
-import Link from "next/link";
 import React, { useContext, useState } from "react";
-import DatePicker from "react-datepicker";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const BookingForm = () => {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
-  const [endDate, setEndDate] = useState<Date | null>(new Date());
   const [extraBed, setExtraBed] = useState<string | null>(null);
-  const handleCalendarClose = () => {};
-  const handleCalendarOpen = () => {};
   const { roomId } = useContext(AppContext) as AppContextType;
   const room = roomAndSuites.find((item) => item.id == roomId);
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    adults: "",
+    extraBed: "",
+    checkin: "",
+    checkout: "",
+    specialRequirements: "",
+  });
 
   if (!room) {
     return <Preloader />;
   }
 
-  const selectHandler = () => {};
+  const totalPrice =
+    room && typeof room.price !== "undefined"
+      ? extraBed === "1"
+        ? room.price + 800
+        : room.price
+      : 0;
 
-const totalPrice = room && typeof room.price !== 'undefined' ? (extraBed === '1' ? room.price + 800 : room.price) : 0;
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    const templateParams = {
+      to_name: "vinod kumar",
+      message: `
+            First Name: ${formData.firstName}
+            Last Name: ${formData.lastName}
+            Email: ${formData.email}
+            Phone: ${formData.phone}
+            Adults: ${formData.adults}
+            Extra Bed: ${formData.extraBed}
+            Checkin: ${formData.checkin}
+            Checkout: ${formData.checkout}
+            Special Requirements: ${formData.specialRequirements}
+          `,
+    };
+    emailjs
+      .send(
+        "service_iy53qdq",
+        "template_xoadkr5",
+        templateParams,
+        "CSM5ny-M2R7aX5vJV"
+      )
+      .then(
+        (response: any) => {
+          console.log("Email sent successfully:", response);
+          Swal.fire({
+            title: "Thanks for Booking",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+
+          setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            adults: "",
+            extraBed: "",
+            checkin: "",
+            checkout: "",
+            specialRequirements: "",
+          });
+        },
+        (error: any) => {
+          console.error("Error sending email:", error);
+        }
+      );
+  };
 
   return (
-    <form action="#">
+    <form onSubmit={handleSubmit}>
       <div className="bd-booking-4__form">
         <div className="bd-booking-4__input mb-15">
           <input type="text" defaultValue={room.title} readOnly />
@@ -34,25 +100,74 @@ const totalPrice = room && typeof room.price !== 'undefined' ? (extraBed === '1'
           <input type="text" value={`Price  ${totalPrice}`} readOnly />
         </div>
         <div className="bd-booking-4__input mb-15">
-          <input type="text" placeholder="First Name" />
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="bd-booking-4__input mb-15">
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
         </div>
         <div className="bd-booking-4__input mb-15">
-          <input type="text" placeholder="Last Name" />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="bd-booking-4__input mb-15">
-          <input type="email" placeholder="Email Address" />
+          <input
+            type="number"
+            placeholder="Phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
         </div>
+
         <div className="bd-booking-4__input mb-15">
-          <input type="email" placeholder="Phone Number" />
+          <label htmlFor="date">Check in</label>
+          <input
+            id="checkin"
+            type="date"
+            name="checkin"
+            value={formData.checkin}
+            onChange={handleChange}
+          />
         </div>
+
+        <div className="bd-booking-4__input mb-15">
+          <label htmlFor="checkout">Check out</label>
+          <input
+            id="checkout"
+            type="date"
+            name="checkout"
+            value={formData.checkout}
+            onChange={handleChange}
+          />
+        </div>
+
         <div className="bd-booking-4__input p-relative mb-15">
           <select
             name="adults"
             id="adults"
             className="bd-nice-select"
-            onChange={selectHandler}
+            value={formData.adults}
+            onChange={handleChange}
           >
-            <option value="" disabled selected>
+            <option value="" disabled>
               Select ADULTS
             </option>
             <option value="1">ADULTS 01</option>
@@ -60,6 +175,7 @@ const totalPrice = room && typeof room.price !== 'undefined' ? (extraBed === '1'
             <option value="3">ADULTS 03</option>
           </select>
         </div>
+
         <div className="bd-booking-4__input p-relative mb-15">
           <select
             name="extrabad"
@@ -74,28 +190,14 @@ const totalPrice = room && typeof room.price !== 'undefined' ? (extraBed === '1'
             <option value="2">No</option>
           </select>
         </div>
-        <div className="bd-booking-4__input mb-15 ryl-block">
-          <i className="flaticon-calendar-2 zindex-5"></i>
-          <DatePicker
-            selected={startDate}
-            onChange={(startDate) => setStartDate(startDate as Date)}
-            onCalendarClose={handleCalendarClose}
-            onCalendarOpen={handleCalendarOpen}
-            className="bd-date-picker"
-          />
-        </div>
-        <div className="bd-booking-4__input mb-15 ryl-block">
-          <i className="flaticon-calendar-2 zindex-5"></i>
-          <DatePicker
-            selected={endDate}
-            onChange={(endDate) => setEndDate(endDate as Date)}
-            onCalendarClose={handleCalendarClose}
-            onCalendarOpen={handleCalendarOpen}
-            className="bd-date-picker bd-booking__input-date"
-          />
-        </div>
+
         <div className="bd-booking-4__input message mb-30">
-          <textarea name="message" placeholder="Special Requirements"></textarea>
+          <textarea
+            name="specialRequirements"
+            placeholder="Special Requirements"
+            value={formData.specialRequirements}
+            onChange={handleChange}
+          ></textarea>
         </div>
         <div className="glo-booking-btn">
           <button type="submit" className="bd-btn dark-btn">
